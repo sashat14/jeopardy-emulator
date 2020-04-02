81a2910d804942776f167b05e0c8910b89041bb8
Sasha <taylork85@gmail.com>
2020-04-01 19:37:51 -0500

    add vuex

---
+++ src/components/PrizeTotal.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h3>Prize Total: ${{prize}}</h3>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name:"PrizeTotal",
+    props:{
+        prize:Number
+    }
+
+}
+</script>
+
+<style>
+
+</style>
